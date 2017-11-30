da5977830864243165d6c294ec87ace2fd0a4a31
ferper <ferpergar456@gmail.com>
2017-11-30 14:28:59 +0100

    changed login

--- app/views.py
+++ app/views.py
@@ -14,3 +14,11 @@ def login():
     return render_template('login.html', 
                            title='Sign In',
                            form=form)
+
+@app.route('/index')
+
+def index():
+	user = {'nickname': 'Miguel'}  # fake user
+	return render_template('index.html',
+                           title='Home',
+                           user=user)
